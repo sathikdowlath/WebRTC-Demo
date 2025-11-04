b91b154d2fd44d6f3e3a95717f77729d4cfe4dc9
Sathik Basha Dowlath Khan <ebmsbasha@gmail.com>
2025-11-04 11:38:52 -0600

    Update server.js

--- server.js
+++ server.js
@@ -12,22 +12,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket => {
   socket.on('join', room => {
+    console.log(`server.js-connection to room: ${room}`);
     socket.join(room);
+    console.log(`server.js-join: ${room}`);
     socket.to(room).emit('ready');
   });
 
   socket.on('offer', (room, offer) => {
     socket.to(room).emit('offer', offer);
+    console.log(`server.js-offer: ${room} - ${offer}`);
   });
 
   socket.on('answer', (room, answer) => {
     socket.to(room).emit('answer', answer);
+    console.log(`server.js-answer: ${room} - ${answer}`);
   });
 
   socket.on('ice-candidate', (room, candidate) => {
     socket.to(room).emit('ice-candidate', candidate);
+    console.log(`server.js-ice-candidate: ${room} - ${candidate}`);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`server.js-Server running on port ${PORT}`));
